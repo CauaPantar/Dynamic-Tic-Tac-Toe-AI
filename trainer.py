17f6b2d5363fe87f038c721d35126e2b85045649
Cauã Pantar <cauapantar207@gmail.com>
2025-09-22 18:35:13 -0300

    feat: Implement RL Agent and training function

--- trainer.py
+++ trainer.py
@@ -1,5 +1,6 @@
 from collections import deque
 import random
+import pickle
 
 
 class Board:
@@ -15,7 +16,6 @@ class Board:
         x_pieces_tuple = tuple(self.pieces['X'])
         o_pieces_tuple = tuple(self.pieces['O'])
         
-        # 3. Combina tudo numa única tupla imutável.
         return (board_tuple, x_pieces_tuple, o_pieces_tuple)
 
 
@@ -30,6 +30,7 @@ class Board:
 
         self.board[row][col] = player
         self.pieces[player].append((row, col))
+        self.check_winner()
         return True
 
     def check_winner(self):
@@ -72,13 +73,25 @@ class Board:
             print("-" * 13)
 
 class Agent:
-    def __init__(self, player_symbol):
+    def __init__(self, player_symbol, filename=None):
         self.player_symbol = player_symbol
-        self.q_table = {}
         self.learning_rate = 0.1
         self.discount_factor = 0.9
-        self.exploration_rate = 1
         self.history = []
+        self.q_table = {}
+        self.games_trained = 0
+        self.exploration_rate = 1.0
+
+        if filename:
+            try:
+                with open(filename, 'rb') as f:
+                    brain_data = pickle.load(f)
+                    self.q_table = brain_data['q_table']
+                    self.games_trained = brain_data['games_trained']
+                    self.exploration_rate = brain_data['exploration_rate']
+                    print(f"Cérebro de {self.player_symbol} carregado! ({self.games_trained} partidas já treinadas)")
+            except FileNotFoundError:
+                print(f"Ficheiro não encontrado. A começar cérebro novo para {self.player_symbol}.")
 
     def choose_action(self, board):
         possible_moves = board.get_possible_moves()
@@ -87,12 +100,14 @@ class Agent:
             return None
 
         state = board.get_state()
+        known_actions = self.q_table.get(state, {})
+        valid_known_actions = {action: value for action, value in known_actions.items() if action in possible_moves}
 
         if random.random() < self.exploration_rate:
             action = random.choice(possible_moves)
         else:
-            if state in self.q_table and self.q_table[state]:
-                action = max(self.q_table[state], key=self.q_table[state].get)
+            if valid_known_actions:
+                action = max(valid_known_actions, key=valid_known_actions.get)
             else:
                 action = random.choice(possible_moves)
 
@@ -101,11 +116,11 @@ class Agent:
     
     def update_q_table(self, reward):
         current_reward = reward
-        
+
         for state, action in reversed(self.history):
             old_q_value = self.get_q_value(state, action)
 
-            new_q_value = old_q_value + self.learning_rate * (current_reward - old_q_value)
+            new_q_value = old_q_value + (self.learning_rate * (current_reward - old_q_value))
 
             self.q_table.setdefault(state, {})
             self.q_table[state][action] = new_q_value
@@ -116,5 +131,87 @@ class Agent:
 
     def get_q_value(self, state, action):
         actions_dict = self.q_table.get(state, {})
-
         return actions_dict.get(action, 0.0)
+
+def train(games_this_session=200000):
+    print(f"Iniciando uma sessão de treino de {games_this_session} partidas...")
+
+    brain_file_x = 'ia_cerebro_X.pkl'
+    brain_file_o = 'ia_cerebro_O.pkl'
+    player_x = Agent(player_symbol='X', filename=brain_file_x)
+    player_o = Agent(player_symbol='O', filename=brain_file_o)
+
+    total_target_games = player_o.games_trained + games_this_session
+    save_points = {
+        int(total_target_games * 0.10): 'ia_nivel_facil.pkl',
+        int(total_target_games * 0.50): 'ia_nivel_medio.pkl',
+        total_target_games: 'ia_nivel_dificil.pkl'
+    }
+    print(f"Novos save points calculados: {save_points}")
+
+    for i in range(1, games_this_session + 1):
+        board = Board()
+        current_player_symbol = 'X'
+        move_counter = 0
+
+        while board.winner is None:
+            current_player = player_x if current_player_symbol == 'X' else player_o
+
+            action = current_player.choose_action(board)
+
+            if action:
+                board.make_move(action[0], action[1], current_player_symbol)
+                move_counter += 1
+            else:
+                break
+
+            if board.check_winner():
+                break
+            
+            if move_counter >= 100:
+                break
+
+            current_player_symbol = 'O' if current_player_symbol == 'X' else 'X'
+
+        player_x.games_trained += 1
+        player_o.games_trained += 1
+
+        if board.winner == 'X':
+            player_x.update_q_table(reward=1)
+            player_o.update_q_table(reward=-1)
+        elif board.winner == 'O':
+            player_x.update_q_table(reward=-1)
+            player_o.update_q_table(reward=1)
+        else: # Empate
+            player_x.update_q_table(reward=0)
+            player_o.update_q_table(reward=0)
+
+        player_x.exploration_rate *= 0.99999
+        player_o.exploration_rate *= 0.99999
+        
+        if i % 10000 == 0:
+            print(f"Sessão atual: Partida {i}/{games_this_session}... (Epsilon atual: {player_o.exploration_rate:.4f})")
+            
+        if player_o.games_trained in save_points:
+            filename = save_points[player_o.games_trained]
+            brain_data_o = {
+                'q_table': player_o.q_table,
+                'games_trained': player_o.games_trained,
+                'exploration_rate': player_o.exploration_rate
+            }
+            with open(filename, 'wb') as f:
+                pickle.dump(brain_data_o, f)
+            print(f"--- Cérebro da IA salvo em '{filename}'! (Total de {player_o.games_trained} partidas) ---")
+
+    final_brain_data_x = {'q_table': player_x.q_table, 'games_trained': player_x.games_trained, 'exploration_rate': player_x.exploration_rate}
+    final_brain_data_o = {'q_table': player_o.q_table, 'games_trained': player_o.games_trained, 'exploration_rate': player_o.exploration_rate}
+    
+    with open(brain_file_x, 'wb') as f:
+        pickle.dump(final_brain_data_x, f)
+    with open(brain_file_o, 'wb') as f:
+        pickle.dump(final_brain_data_o, f)
+        
+    print(f"Treinamento concluído! Cérebros finais guardados.")
+
+if __name__ == '__main__':
+    train()
